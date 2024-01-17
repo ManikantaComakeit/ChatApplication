3a24341d3e90216f5039cb4838f2993039005008
ManikantaComakeit <manikanta.kaspa@comakeit.com>
2024-01-17 11:46:18 +0530

    basic setup is completed

---
+++ chat/src/main/java/com/chatapplication/ChatWebSocket.java
@@ -0,0 +1,11 @@
+package com.chatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebSocket {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWebSocket.class, args);
+    }
+}
